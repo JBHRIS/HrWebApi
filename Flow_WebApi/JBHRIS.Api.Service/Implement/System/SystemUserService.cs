283138d2c186293a2b38282f5e8a2e461ea22fc5
紹安 陳 <aron@jbjob.com.tw>
2021-12-30 10:32:36 +0800

    GetSystemUser

---
+++ Flow_WebApi/JBHRIS.Api.Service/Implement/System/SystemUserService.cs
@@ -0,0 +1,27 @@
+﻿using JBHRIS.Api.Dal.Interface;
+using JBHRIS.Api.Dto.FlowMainInte.Vdb;
+using JBHRIS.Api.Service.Interface.System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JBHRIS.Api.Service.Implement.System
+{
+    public class SystemUserService : SystemUserInterFace
+    {
+        private ISystem_SystemUser_View _ISystem_SystemUser_View;
+        public SystemUserService(ISystem_SystemUser_View system_SystemUser_View)
+        {
+            this._ISystem_SystemUser_View = system_SystemUser_View;
+        }
+
+        public List<SystemUserVdb> GetSystemUser()
+        {
+            return this._ISystem_SystemUser_View.GetSystemUser();
+        }
+       
+
+       
+
+    }
+}
