4eba40fc08953666dbb45d82538109a45902cb04
紹安 陳 <aron@jbjob.com.tw>
2022-02-10 11:23:03 +0800

    更新回報單使用者資訊資料庫結構

--- 員工平台/Portal/Dal/Dao/Share/ShareQuestionUserInfoDao.cs
+++ 員工平台/Portal/Dal/Dao/Share/ShareQuestionUserInfoDao.cs
@@ -37,22 +37,66 @@ namespace Dal.Dao.Share
         /// </summary>
         public void InsertQuestionUserInfo(string CompanyId,string UserId,string AccountCode,int RoleKey,string UserName="",string Email="",string Conetent="")
         {
-            var rQuesitonUserInfo = new QuestionUserInfo();
-            rQuesitonUserInfo.AutoKey = 0;
-            rQuesitonUserInfo.CompanyId = CompanyId;
-            rQuesitonUserInfo.Code = Guid.NewGuid().ToString();
-            rQuesitonUserInfo.AccountCode = AccountCode;
-            rQuesitonUserInfo.UserId = UserId;
-            rQuesitonUserInfo.UserName = UserName;
-            rQuesitonUserInfo.RoleKey = RoleKey;
-            rQuesitonUserInfo.Email = Email;
-            rQuesitonUserInfo.Content = Conetent;
-            rQuesitonUserInfo.DateA = DateTime.Now;
-            rQuesitonUserInfo.DateD = DateTime.MaxValue;
-            rQuesitonUserInfo.Status = "1";
-            rQuesitonUserInfo.InsertMan = AccountCode;
-            rQuesitonUserInfo.InseetDate = DateTime.Now;
-            dcShare.QuestionUserInfo.InsertOnSubmit(rQuesitonUserInfo);
+            var oUserInfo = (from c in dcShare.QuestionUserInfo
+                             where c.CompanyId == CompanyId && c.UserId == UserId
+                             select c).FirstOrDefault();
+            if (oUserInfo == null)
+            {
+                try 
+                {
+                    var rQuesitonUserInfo = new QuestionUserInfo();
+                    rQuesitonUserInfo.AutoKey = 0;
+                    rQuesitonUserInfo.CompanyId = CompanyId;
+                    rQuesitonUserInfo.Code = Guid.NewGuid().ToString();
+                    rQuesitonUserInfo.AccountCode = AccountCode;
+                    rQuesitonUserInfo.UserId = UserId;
+                    rQuesitonUserInfo.UserName = UserName;
+                    rQuesitonUserInfo.RoleKey = RoleKey;
+                    rQuesitonUserInfo.Email = Email;
+                    rQuesitonUserInfo.Content = Conetent;
+                    rQuesitonUserInfo.DateA = DateTime.Now;
+                    rQuesitonUserInfo.DateD = DateTime.MaxValue;
+                    rQuesitonUserInfo.Status = "1";
+                    rQuesitonUserInfo.InsertMan = AccountCode;
+                    rQuesitonUserInfo.InseetDate = DateTime.Now;
+                    dcShare.QuestionUserInfo.InsertOnSubmit(rQuesitonUserInfo);
+                    dcShare.SubmitChanges();
+                }
+                catch (Exception ex)
+                { 
+                
+                }
+                    
+                
+            }
+            else
+            {
+                try
+                {
+                   
+                    oUserInfo.CompanyId = CompanyId;
+                    oUserInfo.Code = oUserInfo.Code;
+                    oUserInfo.AccountCode = AccountCode;
+                    oUserInfo.UserId = UserId;
+                    oUserInfo.UserName = UserName;
+                    oUserInfo.RoleKey = RoleKey;
+                    oUserInfo.Email = Email;
+                    oUserInfo.Content = Conetent;
+                    oUserInfo.DateA = oUserInfo.DateA;
+                    oUserInfo.DateD = oUserInfo.DateD;
+                    oUserInfo.Status = "1";
+                    oUserInfo.InsertMan = oUserInfo.InsertMan;
+                    oUserInfo.InseetDate = oUserInfo.InseetDate;
+                    oUserInfo.UpdateMan = UserName;
+                    oUserInfo.UpdateTime = DateTime.Now;
+                    dcShare.SubmitChanges();
+                }
+                catch (Exception ex)
+                {
+
+                }
+               
+            }
         }
     }
 }
