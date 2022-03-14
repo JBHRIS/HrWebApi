95a593b9e4fc70bdaf8f19f60a696aa6af998c94
tseng <tseng@jbjob.com.tw>
2022-03-14 15:14:00 +0800

    Flow附件上傳補檔

---
+++ 員工平台/Portal/Bll/Flow/Vdb/SaveInPostedFileBaseVdb.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Bll.Flow.Vdb
+{
+    public  class SaveInPostedFileBaseVdb
+    {
+    }
+
+    public class SaveInPostedFileBaseConditions : DataConditions
+    { 
+        public string Company { get; set; }
+        public string InsertMan { get; set; }
+        public HttpPostedFileBase files { get; set; }
+    }
+
+    public class SaveInPostedFileBaseApiRow : StandardDataBaseApiRow
+    {
+        
+
+        public bool Result { get; set; }
+
+    }
+
+    public class SaveInPostedFileBaseRow
+    {
+        public bool Result { get; set; }
+
+    }
+}
