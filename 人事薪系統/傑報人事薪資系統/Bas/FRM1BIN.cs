f193ea25503143a7144fcc08dc82aa3c7aa44e61
AndrewJiang <62090266+AndrewJiang0811@users.noreply.github.com>
2022-03-16 16:57:09 +0800

    修正：詳細科系名稱必填檢核

--- 人事薪系統/傑報人事薪資系統/Bas/FRM1BIN.cs
+++ 人事薪系統/傑報人事薪資系統/Bas/FRM1BIN.cs
@@ -187,6 +187,11 @@ namespace JBHR.Bas
             //        }
             //    }
             //}
+            if (SourceRow.IsNull("詳細科系名稱") && string.IsNullOrWhiteSpace(SourceRow["詳細科系名稱"].ToString()))
+            {
+                TargetRow["錯誤註記"] += string.Format("[詳細科系名稱]為必填欄位.");
+            }
+
             if (!SourceRow.IsNull("入學年月") && !string.IsNullOrWhiteSpace(SourceRow["入學年月"].ToString()) && !check_YYMM(SourceRow["入學年月"].ToString(), out Msg))
             {
                 if (TargetRow.Table != null && TargetRow.Table.Columns.Contains("錯誤註記"))
