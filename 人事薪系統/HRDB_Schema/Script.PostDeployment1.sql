138e13caf8dc11631b38d387d18e0ed963c7b599
Daniel Chih <daniel@jbjob.com.tw>
2022-04-26 17:09:51 +0800

    增加匯入Mtcode出勤種類資料給portal使用

--- 人事薪系統/HRDB_Schema/Script.PostDeployment1.sql
+++ 人事薪系統/HRDB_Schema/Script.PostDeployment1.sql
@@ -12641,7 +12641,41 @@ INSERT INTO [dbo].[NotifyParameterMaster] ([NotifyMasterId], [ParameterCode], [P
 INSERT INTO [dbo].[NotifyParameterMaster] ([NotifyMasterId], [ParameterCode], [ParameterName], [Value], [DataType], [FieldType], [Source], [CreateTime], [CreateMan]) VALUES (5, N'@Comp', N'公司', N'01', N'                                                  ', N'                                                  ', N' ', '20210706 00:00:00.000', N'JB')
 INSERT INTO [dbo].[NotifyParameterMaster] ([NotifyMasterId], [ParameterCode], [ParameterName], [Value], [DataType], [FieldType], [Source], [CreateTime], [CreateMan]) VALUES (5, N'@提醒天數', N'提醒天數', N'7', N'                                                  ', N'                                                  ', N' ', '20210706 00:00:00.000', N'JB')
 
+--匯入Mtcode出勤種類資料給portal使用
+BEGIN
+   IF NOT EXISTS (select * from MTCODE where CATEGORY = 'Portal_AttendType' and  CODE = 'AttendType_Attend')
+   BEGIN
+       INSERT [dbo].[MTCODE] ([CATEGORY], [CODE], [NAME], [SORT], [DISPLAY]) VALUES (N'Portal_AttendType', N'AttendType_Attend', N'出勤', 1, 1)
+   END
+END
 
+BEGIN
+   IF NOT EXISTS (select * from MTCODE where CATEGORY = 'Portal_AttendType' and  CODE = 'AttendType_Card')
+   BEGIN
+       INSERT [dbo].[MTCODE] ([CATEGORY], [CODE], [NAME], [SORT], [DISPLAY]) VALUES (N'Portal_AttendType', N'AttendType_Card', N'刷卡', 2, 1)
+   END
+END
+
+BEGIN
+   IF NOT EXISTS (select * from MTCODE where CATEGORY = 'Portal_AttendType' and  CODE = 'AttendType_Abs')
+   BEGIN
+       INSERT [dbo].[MTCODE] ([CATEGORY], [CODE], [NAME], [SORT], [DISPLAY]) VALUES (N'Portal_AttendType', N'AttendType_Abs', N'請假', 3, 1)
+   END
+END
+
+BEGIN
+   IF NOT EXISTS (select * from MTCODE where CATEGORY = 'Portal_AttendType' and  CODE = 'AttendType_Ot')
+   BEGIN
+       INSERT [dbo].[MTCODE] ([CATEGORY], [CODE], [NAME], [SORT], [DISPLAY]) VALUES (N'Portal_AttendType', N'AttendType_Ot', N'加班', 4, 1)
+   END
+END
+
+BEGIN
+   IF NOT EXISTS (select * from MTCODE where CATEGORY = 'Portal_AttendType' and  CODE = 'AttendType_Abnormal')
+   BEGIN
+       INSERT [dbo].[MTCODE] ([CATEGORY], [CODE], [NAME], [SORT], [DISPLAY]) VALUES (N'Portal_AttendType', N'AttendType_Abnormal', N'異常', 5, 1)
+   END
+END
 
 
 --更新key_man
